aa51177fc79b089ec98ecd9b54d56ef198c4e734
Yggdrass <78912844+Yggdrass@users.noreply.github.com>
2023-10-05 19:21:30 +0200

    Added CartPage, fixed the list so it can direct the user via a product to the product details page and display the products information.

--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Product from "../../pages/ProductDetails";
+import ProductDetails from "../../pages/ProductDetails";
 
 const ProductCard = () => {
   const [products, setProducts] = useState([]);
   return (
-    <Link to="/product/:id" element={<Product />}>
-      {products.map((product) => (
-        <div className="card">
-          <h2>{product.title}</h2>
-          <img src={product.imageUrl} alt={product.title} />
-          <p>{product.description}</p>
-        </div>
-      ))}
-    </Link>
+    <li>
+      <Link to="/product/${item.id}" element={<ProductDetails />}>
+        {products.map((product) => (
+          <div className="card">
+            <h2>{product.title}</h2>
+            <img src={product.imageUrl} alt={product.title} />
+            <p>{product.description}</p>
+          </div>
+        ))}
+      </Link>
+    </li>
   );
 };
 
