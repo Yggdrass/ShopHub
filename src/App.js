0d14584c32ce121f01ab8eed46b64d865c4ee3eb
Yggdrass <78912844+Yggdrass@users.noreply.github.com>
2023-10-13 14:00:54 +0200

    Stopped the cartitem in the cartpage from being decremented below zero.

--- src/App.js
+++ src/App.js
@@ -4,21 +4,14 @@ import font from "./App.module.css";
 import Home from "./pages/Home.jsx";
 import ProductDetails from "./pages/ProductDetails.jsx";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import CartPage, {
-  CartItemIcon,
-  cartFromLocalStorage,
-} from "./pages/CartPage.jsx";
-import { createContext, useEffect, useState } from "react";
+import CartPage, { CartItemIcon } from "./pages/CartPage.jsx";
+import { createContext } from "react";
 import ContactPage from "./pages/ContactPage.jsx";
 import ContactFormSubmitSuccess from "./pages/ContactFormSubmitSuccess.jsx";
 
 export const CartContext = createContext([]);
 
 function App() {
-  const [cart, setCart] = useState([]);
-  // Use useEffect to check for localstorage cart, if there is then load that info
-
-  const value = { cart, setCart };
   return (
     <div className={font.FamilyQuicksand}>
       <CartContext.Provider value={<CartItemIcon />}>
